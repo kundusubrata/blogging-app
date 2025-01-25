751b9680a2f661b2bf6e3cca573b3643936ff917
kundusubrata <kundu.subrata2020@outlook.com>
2025-01-25 15:02:39 +0530

    tailwindcss-shadcn-vite setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className="text-3xl underline font-bold text-center">Hello</h1>
+      <Button variant="default">Click me</Button>
+    </div>
+  )
+}
+
+export default App
