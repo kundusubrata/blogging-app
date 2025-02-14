66beb1c061509074bab7ed8f83f2e13e28ea4fda
kundusubrata <kundu.subrata2020@outlook.com>
2025-02-14 16:18:31 +0530

    bug fix on bcrypt

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import prisma from "../config/prisma";
